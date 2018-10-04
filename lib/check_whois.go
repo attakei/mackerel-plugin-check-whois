ecd270be9f944d3ad819ebf6af78c0cf08ebbd42
Kazuya Takei <attakei@gmail.com>
2018-10-04 17:52:35 +0900

    Use domainr/whois

--- lib/check_whois.go
+++ lib/check_whois.go
@@ -2,8 +2,10 @@ package check_whois
 
 import (
     "flag"
+    "fmt"
     "os"
 
+    "github.com/domainr/whois"
     "github.com/mackerelio/checkers"
 )
 
@@ -20,5 +22,21 @@ func run(args []string) * checkers.Checker {
          d = flag.String("domain", "example.com", "check target domain")
     )
     flag.Parse()
+    request, err := whois.NewRequest(*d)
+    if err != nil {
+        // TODO: Echo reason
+        os.Exit(1)
+    }
+    response, err := whois.DefaultClient.Fetch(request)
+    if err != nil {
+        // TODO: Echo reason
+        os.Exit(1)
+    }
+    record, err := response.Parse() // not implemented yet
+    if err != nil {
+        // TODO: Echo reason
+        os.Exit(1)
+    }
+    fmt.Println(record)
 	return checkers.NewChecker(checkers.OK, *d)
 }
