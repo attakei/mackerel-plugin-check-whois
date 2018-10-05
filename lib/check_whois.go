8583914c92e5876b98e3eab902fc91c53c27af07
Kazuya Takei <attakei@gmail.com>
2018-10-06 01:44:44 +0900

    Split parse expirationdata

--- lib/check_whois.go
+++ lib/check_whois.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "os"
     "strings"
+    "time"
 
     "github.com/joyt/godate"
     "github.com/likexian/whois-go"
@@ -34,29 +35,29 @@ func fetchWhois(domain string) (raw string, err error) {
 }
 
 
+func fetchExpired(domain string) (expired time.Time, err error) {
+    raw, err := fetchWhois(domain)
+    if err != nil {
+        return time.Now(), err
+    }
+    record, err := whois_parser.Parse(raw)
+    if err != nil {
+        return time.Now(), err
+    }
+    return date.Parse(record.Registrar.ExpirationDate)
+}
+
+
 func run(args []string) * checkers.Checker {
     var (
          d = flag.String("domain", "example.com", "check target domain")
     )
     flag.Parse()
-    raw, err := fetchWhois(*d)
+    expired, err := fetchExpired(*d)
     if err != nil {
         // TODO: handle more formal for golang
         fmt.Println(err)
         os.Exit(1)
     }
-    record, err := whois_parser.Parse(raw)
-    if err != nil {
-        // TODO: handle more formal for golang
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    expired, err := date.Parse(record.Registrar.ExpirationDate)
-    if err != nil {
-        // TODO: handle more formal for golang
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    fmt.Println(expired)
 	return checkers.NewChecker(checkers.OK, *d + " is expired at " + expired.String())
 }
